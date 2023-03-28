91e984da44c8fca9e20da3e5d5caa0c4df425b69
samdaphbynet <lj8@hotmail.fr>
2023-03-28 23:18:43 +0000

    update function _printf

--- _printf.c
+++ _printf.c
@@ -15,7 +15,7 @@ int _printf(const char *format, ...)
 	va_list args;
 	va_start(args, format);
 
-	if (&format[i] == NULL)
+	if (&format == NULL || (format[0] == '%' && format[1] == '\0'))
 	{
 		return (-1);
 	}
