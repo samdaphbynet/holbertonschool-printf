8f751a0902a4307578478c866467130cae8d752f
samdaphbynet <lj8@hotmail.fr>
2023-03-28 14:29:43 +0000

    function _printf that works exactly like printf

---
+++ _printf.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	int count = 0;
+	int value = 0;
+	int i = 0;
+	int (*f)(va_list);
+
+	if (&format[i] == NULL)
+	{
+		return (-1);
+	}
+
+	while (format[i])
+	{
+		if (format[i] != '%')
+		{
+			value = write(1, &format[i], 1);
+			count = count + value;
+			i++;
+			continue;
+		}
+
+		if (format[i] == '%')
+		{
+			f = specifiers(&format[i + 1]);
+		}
+
+		if (f != NULL)
+		{
+			value = f(args);
+			count = count + value;
+			i = i + 2;
+			continue;
+		}
+
+		if (format[i + 1] == '\0')
+		{
+			break;
+		}
+
+		if (format[i + 1] != '\0')
+		{
+			value = write(1, &format[i], 1);
+			count = count + value;
+			i = i + 2;
+			continue;
+		}
+	}
+	va_end(args);
+	return count;
+}
