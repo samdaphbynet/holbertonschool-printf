ab9e1a93aece56ee85a2e4d2a043b6210120a27e
samdaphbynet <lj8@hotmail.fr>
2023-03-31 21:55:08 +0000

    function that converted to binary

---
+++ print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_binary - function that converted to binary.
+ * @args: argument passed in function _printf
+ * Return: number converted
+ */
+
+int print_binary(va_list args)
+{
+	int n = va_arg(args, int);
+	int binary[40];
+	int i = 0;
+
+	if (n == 0)
+	{
+		write(1, "0", 1);
+		return (-1);
+	}
+
+	for (; n > 0; )
+	{
+		binary[i++] = n % 2;
+		n /= 2;
+	}
+
+	for (j = 0; i-1; j >= 0; j--)
+		_printf("%d", binary[j]);
+	return (n);
+}
